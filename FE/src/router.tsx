e8bc50bbc1c2fb868b93c1bb9e3762624c0290c8
kim seong won <kimsw0516@naver.com>
2024-07-29 16:03:52 +0900

    fix: Router파일 주석 변경

--- FE/src/router.tsx
+++ FE/src/router.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
-<<<<<<< FE/src/router.tsx
 import Overview from './pages/Overview/';
-=======
->>>>>>> FE/src/router.tsx
+
 
 const router = createBrowserRouter([
     {
