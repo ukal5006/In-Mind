83d963c0d29d5ae0eb8ba3565f53235c64e7ecbb
Leo Lee <ukal5006@naver.com>
2024-07-29 13:07:00 +0900

    feat : router 설정

--- FE/src/index.tsx
+++ FE/src/index.tsx
@@ -1,11 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import { GlobalStyle } from './GlobalStyle';
-import Overview from './pages/Overview';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <>
         <GlobalStyle />
-        <Overview />
+        <RouterProvider router={router} />
     </>
 );
