911e58e914c313989fe0f7efe05d70e3ea7494b3
Leo Lee <ukal5006@naver.com>
2024-07-29 15:45:31 +0900

    feat : Components에서 상속받는 컴포넌트 생성

---
+++ FE/src/pages/Overview/LoginBtn.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { colors } from '../../theme/colors';
+import Btn from '../../components/Btn';
+
+const LoginBtn = styled(Btn)`
+    height: 50px;
+    width: 700px;
+    background-color: ${colors.blue};
+    color: ${colors.white};
+    font-size: 20px;
+    font-weight: 700;
+    margin-top: 10px;
+`;
+
+export default LoginBtn;
---
+++ FE/src/pages/Overview/OnePageWrapper.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import Wrapper from '../../components/Wrapper';
+
+const OnePageWrapper = styled(Wrapper)`
+    width: 100vw;
+    min-width: 750px;
+    height: 100vh;
+    flex-direction: column;
+`;
+
+export default OnePageWrapper;
