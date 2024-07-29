1975e19804e92fd3615a78859643f071ea89ec3c
Leo Lee <ukal5006@naver.com>
2024-07-29 15:43:41 +0900

    feat : Wrapper 컴포넌트 생성

---
+++ FE/src/components/Wrapper.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+    padding: 10px;
+`;
+
+export default Wrapper;
