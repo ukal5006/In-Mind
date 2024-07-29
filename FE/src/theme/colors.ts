68090f3d729d4abf822cb6c1ee66103422416831
Leo Lee <ukal5006@naver.com>
2024-07-29 13:05:28 +0900

    feat: colors.ts 파일 추가 - 색상 추가

---
+++ FE/src/theme/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+    blue: '#03a3fd',
+    red: '#e83131',
+    lightGreen: '#c6eedb',
+    green: '#2db501',
+    yellow: '#ffd800',
+    white: '#f7f7f7',
+    darkGray: '#757575',
+};
