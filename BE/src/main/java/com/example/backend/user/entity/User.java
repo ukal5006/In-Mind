ce8c39bf147d68a2b86f59efb0128a3bf886e0d4
유현진 <a92046503@gmail.com>
2024-07-30 17:38:23 +0900

    [BE] feat : User Entity 생성
    User, Certificate, Resume, Organization Entity 생성

---
+++ BE/src/main/java/com/example/backend/user/entity/User.java
@@ -0,0 +1,62 @@
+package com.example.backend.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor (access = AccessLevel.PROTECTED)
+@Table(name = "user")
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idx")
+    private Long id;
+
+    //FK
+    @OneToOne
+    private Organization org;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String nickname;
+
+    @Column(nullable = false)
+    private String tel;
+
+    private String profile;
+
+    @Column(name = "is_auth")
+    private boolean isAuth;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RoleStatus role;
+
+    @Column(name = "is_alive", nullable = false)
+    @ColumnDefault("true")
+    private boolean isAlive;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
