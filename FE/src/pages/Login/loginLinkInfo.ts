7e467a21fa2ecb750509c9cb5129480b57d41c1b
Leo Lee <ukal5006@naver.com>
2024-07-30 15:40:05 +0900

    refactor : loginLinkInfo 변경
    - 회원가입의 link 변경

--- FE/src/pages/Login/loginLinkInfo.ts
+++ FE/src/pages/Login/loginLinkInfo.ts
@@ -6,7 +6,7 @@ interface LoginLink {
 const loginLinkInfo: LoginLink[] = [
     { title: '비밀번호 찾기', link: '/findPw' },
     { title: '아이디 찾기', link: '/findId' },
-    { title: '회원가입', link: '/join' },
+    { title: '회원가입', link: '/join/user' },
 ];
 
 export default loginLinkInfo;
