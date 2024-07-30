3302d2d7e97f3e37db9a33ac68b522069c4ea3b0
Leo Lee <ukal5006@naver.com>
2024-07-30 12:46:25 +0900

    feat : BigLogo 컴포넌트에 이벤트 추가
    - BigLogo 컴포넌트를 누르면 홈으로 이동하는 기능 추가

--- FE/src/components/BigLogo.tsx
+++ FE/src/components/BigLogo.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import Logo from './Logo';
+import { useNavigate } from 'react-router-dom';
 
 const StyledLogo = styled(Logo)`
     font-size: 40px;
@@ -7,6 +8,12 @@ const StyledLogo = styled(Logo)`
 `;
 
 function BigLogo() {
-    return <StyledLogo>In Mind</StyledLogo>;
+    const navigate = useNavigate();
+
+    const handleLogoClick = () => {
+        navigate('/');
+    };
+
+    return <StyledLogo onClick={() => handleLogoClick()}>In Mind</StyledLogo>;
 }
 export default BigLogo;
