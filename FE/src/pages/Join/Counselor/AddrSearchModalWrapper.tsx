fc908cab9c186f9504c5a26baa16976e9d8e6247
Leo Lee <ukal5006@naver.com>
2024-07-30 15:40:49 +0900

    feat : AddrSearchModalWrapper 생성

---
+++ FE/src/pages/Join/Counselor/AddrSearchModalWrapper.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import Wrapper from '../../../components/Container';
+
+const AddrSearchModalWrapper = styled(Wrapper)`
+    width: 430px;
+    height: 430px;
+`;
+
+export default AddrSearchModalWrapper;
