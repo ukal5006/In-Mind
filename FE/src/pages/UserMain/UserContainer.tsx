adc2cb1f3c8f9de57b5839c1530013cb91b86348
Leo Lee <ukal5006@naver.com>
2024-07-31 15:24:06 +0900

    feat : UserContainer 생성

---
+++ FE/src/pages/UserMain/UserContainer.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import Container from '../../components/Container';
+
+const UserContainer = styled(Container)`
+    flex-direction: column;
+    width: 100vw;
+    height: 100vh;
+    min-width: 750px;
+    justify-content: start;
+    box-sizing: border-box;
+`;
+
+export default UserContainer;
