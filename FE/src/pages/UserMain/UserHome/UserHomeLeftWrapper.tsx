4307a3fe94d6f167cb19c1a9d334488954107281
Leo Lee <ukal5006@naver.com>
2024-07-31 15:24:59 +0900

    feat : UserHomeLeftWrapper 생성

---
+++ FE/src/pages/UserMain/UserHome/UserHomeLeftWrapper.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import Wrapper from '../../../components/Wrapper';
+
+const UserHomeLeftWrapper = styled(Wrapper)`
+    width: 50%;
+    height: 100%;
+    box-sizing: border-box;
+`;
+
+export default UserHomeLeftWrapper;
