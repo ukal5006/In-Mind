1437892c3a5e6f22456b330a8eb49c4bd1a3f958
poow810 <poow810@naver.com>
2024-07-31 12:51:28 +0900

    [BE] feat : 좋아요, 리뷰 엔티티 생성
    ì¢Like, Review 모델 생성 및 관계 설정

---
+++ BE/src/main/java/com/example/backend/user/entity/Like.java
@@ -0,0 +1,33 @@
+package com.example.backend.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "like")
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idx")
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_idx", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "co_idx", nullable = false)
+    private User counselor;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
