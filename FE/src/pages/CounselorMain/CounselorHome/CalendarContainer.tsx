c43d2d008ec438ee2d154fcaa21ef693dad5846e
Leo Lee <ukal5006@naver.com>
2024-07-31 15:22:52 +0900

    feat : CalendarContainer 생성

---
+++ FE/src/pages/CounselorMain/CounselorHome/CalendarContainer.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import Container from '../../../components/Container';
+
+const CalendarContainer = styled(Container)`
+    height: 90%;
+    width: 60%;
+    border: 1px solid black;
+    border-radius: 10px;
+`;
+
+export default CalendarContainer;
