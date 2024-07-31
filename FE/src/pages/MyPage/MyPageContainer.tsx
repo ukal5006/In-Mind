62d43cb0d6c79759004e46bb1de4efea491d1fb5
Leo Lee <ukal5006@naver.com>
2024-07-31 15:23:22 +0900

    feat : MypageContainer 생성

---
+++ FE/src/pages/MyPage/MyPageContainer.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import Container from '../../components/Container';
+
+const MyPageContainer = styled(Container)`
+    width: 100vw;
+    height: calc(100vh - 80px);
+    justify-content: space-evenly;
+`;
+
+export default MyPageContainer;
