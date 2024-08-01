01d8b4eea7bc6637e3610cefa1fa1c4912e1fa55
유현진 <a92046503@gmail.com>
2024-08-01 12:54:21 +0900

    [BE] error: pull error 발생
     BE pull 할 때 error 발생하여 organization branch commit함

--- BE/src/main/java/com/example/backend/user/controller/OrganizationController.java
+++ BE/src/main/java/com/example/backend/user/controller/OrganizationController.java
@@ -1,7 +1,30 @@
 package com.example.backend.user.controller;
 
+import com.example.backend.user.dto.OrgRequestDTO;
+import com.example.backend.user.service.OrganizationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/orgs")
 public class OrganizationController {
+
+    private final OrganizationService orgService;
+
+    @PostMapping
+    public ResponseEntity<?> addOrganization(@RequestBody OrgRequestDTO requestDTO) {
+        log.info(requestDTO.toString());
+        orgService.saveOrg(requestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
 }
--- BE/src/main/java/com/example/backend/user/service/OrganizationService.java
+++ BE/src/main/java/com/example/backend/user/service/OrganizationService.java
@@ -1,7 +1,51 @@
 package com.example.backend.user.service;
 
+import com.example.backend.user.dto.OrgRequestDTO;
+import com.example.backend.user.entity.Organization;
+import com.example.backend.user.repository.OrganizationRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
+@RequiredArgsConstructor
 public class OrganizationService {
+
+    private final OrganizationRepository orgRepository;
+
+    public void saveOrg(OrgRequestDTO orgRequestDTO) {
+        Organization org = toEntity(orgRequestDTO);
+        orgRepository.save(org);
+    }
+
+    private Organization toEntity(OrgRequestDTO orgRequestDTO) {
+        return Organization.builder()
+                .name(orgRequestDTO.getName())
+                .addr(orgRequestDTO.getAddr())
+                .tel(orgRequestDTO.getTel())
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    private OrgRequestDTO fromEntity(Organization organization) {
+        return new OrgRequestDTO(organization.getName(), organization.getAddr(), organization.getTel());
+    }
+//
+//    public UserDTO getUser(Long id) {
+//        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+//        return convertToDTO(user);
+//    }
+//
+//    private User convertToEntity(UserDTO userDTO) {
+//        return User.builder()
+//                .id(userDTO.getId())
+//                .name(userDTO.getName())
+//                .email(userDTO.getEmail())
+//                .build();
+//    }
+//
+//    private UserDTO convertToDTO(User user) {
+//        return new UserDTO(user.getId(), user.getName(), user.getEmail());
+//    }
 }
