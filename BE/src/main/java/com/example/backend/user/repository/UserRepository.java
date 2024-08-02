2928a467eb8d8c74d244932fe3873c5a58ca0bdb
유현진 <a92046503@gmail.com>
2024-08-02 15:42:32 +0900

    [BE]hotfix: UserRepository 수정
    int -> Long으로 변경 완료

--- BE/src/main/java/com/example/backend/user/repository/UserRepository.java
+++ BE/src/main/java/com/example/backend/user/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.example.backend.user.repository;
 import com.example.backend.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
