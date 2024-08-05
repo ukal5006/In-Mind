cab84091c97da373f20351d80e30b67133ab0ccc
Leo Lee <ukal5006@naver.com>
2024-08-05 13:14:14 +0900

    feat : 상담사 테스트 데이터 추가

--- FE/src/testData/userInfo.ts
+++ FE/src/testData/userInfo.ts
@@ -1,11 +1,21 @@
+// const userInfo = {
+//     name: '홍길동',
+//     email: 'user@naver.com',
+//     phone: '010-1234-5678',
+//     nickName: '삼성맨',
+//     created_at: '2024.08.01',
+//     updated_at: '2024.08.01',
+//     role: 'user',
+// };
+
 const userInfo = {
-    name: '홍길동',
-    email: 'user@naver.com',
+    name: '오은영',
+    email: 'counselor@naver.com',
     phone: '010-1234-5678',
     nickName: '삼성맨',
     created_at: '2024.08.01',
     updated_at: '2024.08.01',
-    role: 'user',
+    role: 'counselor',
 };
 
 export default userInfo;
