095dad15fe7030f1d1fc5d64386e782e897e0f75
Leo Lee <ukal5006@naver.com>
2024-08-05 13:16:16 +0900

    style : 최대 너비 설정

--- FE/src/pages/CounselorMain/index.tsx
+++ FE/src/pages/CounselorMain/index.tsx
@@ -1,12 +1,20 @@
 import { Outlet } from 'react-router-dom';
 import CounselorContainer from './CounselorContainer';
 import Nav from '../../components/Nav';
+import styled from 'styled-components';
+import Wrapper from '../../components/Wrapper';
+
+const CounselorWrapper = styled(Wrapper)`
+    max-width: 1200px;
+`;
 
 function CounselorMain() {
     return (
         <CounselorContainer>
             <Nav />
-            <Outlet />
+            <CounselorWrapper>
+                <Outlet />
+            </CounselorWrapper>
         </CounselorContainer>
     );
 }
