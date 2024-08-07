fff4537641e6dacb86e1792599cf73d509fc5e59
poow810 <poow810@naver.com>
2024-08-07 13:52:23 +0900

    [BE] feat : 트랜잭션 추가
    Create, Update, Deleteì에 대한 transactional 추가

--- BE/src/main/java/com/ssafy/inmind/notification/service/SseEmitterService.java
+++ BE/src/main/java/com/ssafy/inmind/notification/service/SseEmitterService.java
@@ -5,6 +5,7 @@ import com.ssafy.inmind.notification.dto.NotificationDto;
 import com.ssafy.inmind.notification.repository.EmitterRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
