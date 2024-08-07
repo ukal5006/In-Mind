4e8d8f8b6c3f8a7c99e99f1b5d0e3a39f285738f
poow810 <poow810@naver.com>
2024-08-07 13:29:43 +0900

    [BE] feat : 분석 결과 목록 조회 기능
    - 사용자가 자신의 아이에 대한 분석 결과 목록을 조회할 수 있다.

--- BE/src/main/java/com/ssafy/inmind/config/SwaggerConfig.java
+++ BE/src/main/java/com/ssafy/inmind/config/SwaggerConfig.java
@@ -11,7 +11,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .addServersItem(new Server().url("/api"))
+                .addServersItem(new Server().url("/"))
                 .components(new Components())
                 .info(apiInfo());
     }
