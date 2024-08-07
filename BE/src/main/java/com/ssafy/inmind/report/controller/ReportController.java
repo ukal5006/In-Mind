12a1808f0f9a4f56bcf083faa6e17e0473b2efe9
유현진 <a92046503@gmail.com>
2024-08-07 12:35:41 +0900

    [BE] feat: 분석결과 조회 기능 구현
    분석 결과 조회 기능 구현

--- BE/src/main/java/com/ssafy/inmind/report/controller/ReportController.java
+++ BE/src/main/java/com/ssafy/inmind/report/controller/ReportController.java
@@ -1,10 +1,14 @@
 package com.ssafy.inmind.report.controller;
 
 import com.ssafy.inmind.report.dto.ReportRequestDto;
+import com.ssafy.inmind.report.dto.ReportResponseDto;
 import com.ssafy.inmind.report.service.ReportService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,8 +19,13 @@ import org.springframework.web.bind.annotation.*;
 public class ReportController {
 
     private final ReportService reportService;
+    
+    @Operation(summary = "검사 분석 조회", description = "검사 분석을 조회합니다.")
+    @GetMapping("/{reportId}")
+    public ResponseEntity<ReportResponseDto> getReport(@PathVariable @Parameter(description = "검사분석번호") Long reportId) {
+        ReportResponseDto report = reportService.getReport(reportId);
+        return ResponseEntity.ok(report);
+    }
 
 
-
-                
 }
