31233d53e802e6ad49a6ca35eadc38c369b28d09
poow810 <poow810@naver.com>
2024-08-07 15:25:51 +0900

    [BE] feat : 화상 상담 기록 저장
    화상 상담이 종료되면 해당 이력이 저장됩니다.

--- BE/src/main/java/com/ssafy/inmind/consulting/service/ConsultingService.java
+++ BE/src/main/java/com/ssafy/inmind/consulting/service/ConsultingService.java
@@ -1,5 +1,6 @@
 package com.ssafy.inmind.consulting.service;
 
+import com.ssafy.inmind.consulting.dto.ConsultingRequestDto;
 import com.ssafy.inmind.consulting.dto.ConsultingResponseDto;
 import com.ssafy.inmind.consulting.entity.History;
 import com.ssafy.inmind.consulting.repository.ConsultingRepository;
@@ -9,6 +10,7 @@ import com.ssafy.inmind.reservation.entity.Reservation;
 import com.ssafy.inmind.reservation.repository.ReserveRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -31,8 +33,18 @@ public class ConsultingService {
                 .collect(Collectors.toList());
     }
 
-    public void saveConsulting(ConsultingResponseDto responseDto) {
-        Reservation reservation = reserveRepository.findById(responseDto.getId())
+    @Transactional
+    public void saveConsulting(ConsultingRequestDto requestDTO) {
+        Reservation reservation = reserveRepository.findById(requestDTO.getReserveID())
                 .orElseThrow(() -> new RestApiException(ErrorCode.BAD_REQUEST));
+
+        History history = History.builder()
+                .reservation(reservation)
+                .date(requestDTO.getDate())
+                .startTime(requestDTO.getStartTime())
+                .endTime(requestDTO.getEndTime())
+                .build();
+
+        consultingRepository.save(history);
     }
 }
