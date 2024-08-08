19be62b71d63f79f618e3c96d860e50c2b52b40b
Leo Lee <ukal5006@naver.com>
2024-08-08 17:50:37 +0900

    feat : 이력 테스트중

--- FE/src/apis/managementApi.ts
+++ FE/src/apis/managementApi.ts
@@ -2,7 +2,11 @@ import ENDPOINT from './endpoint';
 
 const MANAGEMENTDEFAULT = `${ENDPOINT}/management`; // 상담 API 기본
 
-export const DEFAULTTIME = `${MANAGEMENTDEFAULT}/default-time`; //기본 상담시간 생성
-export const CREATEUNAVAILABLETIME = `${MANAGEMENTDEFAULT}/unavailable-time`; //기본 상담시간 생성
-export const DELETEUNAVAILABLETIME = (idx: number) => `${MANAGEMENTDEFAULT}/unavailable-time?unavailableTimeIdx=${idx}`; //기본 상담시간 삭제
-export const READUNAVAILABLETIME = (counselorId: number) => `${MANAGEMENTDEFAULT}/unavailable-time/${counselorId}`; //기본 상담시간 생성, 삭제
+const DEFAULTTIME = `${MANAGEMENTDEFAULT}/default-time`;
+export const RDDEFAULTTIME = (userId: number) => `${DEFAULTTIME}?userId=${userId}`; //기본 상담시간 조회, 삭제
+export const UPDATEDEFAULTTIME = `${DEFAULTTIME}`; //기본 상담시간 수정
+
+const UNAVAILABLE = `${MANAGEMENTDEFAULT}/unavailable-time`;
+export const CREATEUNAVAILABLETIME = `${UNAVAILABLE}`; //상담 불가능 시간 생성
+export const DELETEUNAVAILABLETIME = (idx: number) => `${UNAVAILABLE}?unavailableTimeIdx=${idx}`; //상담 불가능 시간 제거
+export const READUNAVAILABLETIME = (counselorId: number) => `${UNAVAILABLE}/${counselorId}`; //상담 불가능 시간 조회
--- FE/src/pages/CounselorMain/CounselorHome/ReservationListContainer.tsx
+++ FE/src/pages/CounselorMain/CounselorHome/ReservationListContainer.tsx
@@ -4,8 +4,14 @@ import Container from '../../../components/Container';
 const ReservationListContainer = styled(Container)`
     height: 90%;
     width: 30%;
+    padding: 0px;
     border: 1px solid black;
     border-radius: 10px;
+    position: relative;
+    flex-direction: column;
+    justify-content: flex-start;
+    box-sizing: border-box;
+    overflow-y: scroll;
 `;
 
 export default ReservationListContainer;
