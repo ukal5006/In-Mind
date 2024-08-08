a4e4e47d3564a6c0ce98dac4c0a902e52e98ca2d
김종원 <roista57@gmail.com>
2024-08-08 11:20:18 +0900

    [BE] feat:  비밀번호 변경, 회원 정보 수정, 상담사 회원 정보 수정 기능 추가

--- BE/src/main/java/com/ssafy/inmind/user/dto/CounselorUpdateRequestDto.java
+++ BE/src/main/java/com/ssafy/inmind/user/dto/CounselorUpdateRequestDto.java
@@ -9,11 +9,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CounselorUpdateRequestDto {
-    private Long userIdx;
-
     private String userEmail;
-
-    private String userPassword;
 
     private String userName;
 
@@ -21,23 +17,7 @@ public class CounselorUpdateRequestDto {
 
     private String userProfile;
 
-    private String userIsAuth;
-
     private RoleStatus userRole;
 
-    private String userIsLive;
-
-    public static CounselorUpdateRequestDto fromEntity(User user) {
-        return CounselorUpdateRequestDto.builder()
-                .userIdx(user.getId())
-                .userEmail(user.getEmail())
-                .userName(user.getPassword())
-                .userName(user.getName())
-                .userTel(user.getTel())
-                .userIsAuth(user.getIsAuth())
-                .userProfile(user.getProfile())
-                .userRole(user.getRole())
-                .userIsLive(user.getIsAlive())
-                .build();
-    }
+    private String intro;
 }
