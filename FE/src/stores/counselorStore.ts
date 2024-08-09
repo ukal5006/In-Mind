2718bfeccac41717d4061d5300dc459f09a2591e
Leo Lee <ukal5006@naver.com>
2024-08-09 17:47:35 +0900

    feat : 상담 예약

--- FE/src/stores/counselorStore.ts
+++ FE/src/stores/counselorStore.ts
@@ -1,64 +1,59 @@
 import { create } from 'zustand';
 import axios from 'axios';
-import {USERDEFAULT} from '../apis/userApi'
-import { useEffect } from 'react';
-interface Counselor {
-  idx: number;
-  name: string;
-  tel:string;
-  profile:string;
-  IsAuth:string;
-  intro:string;
-  organizationName: string;
-  organizationTel: string;
-  reviewCount: number;
-  reviewAverage: number;
+import { USERDEFAULT } from '../apis/userApi';
+// import { useEffect } from 'react';
+export interface Counselor {
+    userIdx: number;
+    name: string;
+    tel: string;
+    profile: string;
+    IsAuth: string;
+    intro: string;
+    organizationName: string;
+    organizationTel: string;
+    reviewCount: number;
+    reviewAverage: number;
 }
 
 interface CounselorState {
-  counselors: Counselor[];
-  filteredCounselors: Counselor[];
-  isLoading: boolean;
-  error: string | null;
-  currentPage: number;
-  searchType: 'name' | 'organization';
-  searchTerm: string;
-  filterOption: 'review' | 'reviewAverage' | 'distance';
-  fetchCounselors: (name:string|null) => Promise<void>;
-  setSearchType: (type: 'name' | 'organization') => void;
-  setSearchTerm: (term: string) => void;
-  setFilterOption: (option: 'review' | 'reviewAverage' | 'distance') => void;
-  setCurrentPage: (page: number) => void;
+    counselors: Counselor[];
+    filteredCounselors: Counselor[];
+    isLoading: boolean;
+    error: string | null;
+    currentPage: number;
+    searchType: 'name' | 'organization';
+    searchTerm: string;
+    filterOption: 'review' | 'reviewAverage' | 'distance';
+    fetchCounselors: (name: string | null) => Promise<void>;
+    setSearchType: (type: 'name' | 'organization') => void;
+    setSearchTerm: (term: string) => void;
+    setFilterOption: (option: 'review' | 'reviewAverage' | 'distance') => void;
+    setCurrentPage: (page: number) => void;
 }
 
-
-
-
 const useCounselorStore = create<CounselorState>((set, get) => ({
-  counselors: [],
-  filteredCounselors: [],
-  isLoading: false,
-  error: null,
-  currentPage: 1,
-  searchType: 'name',
-  searchTerm: '',
-  filterOption: 'review',
-  
+    counselors: [],
+    filteredCounselors: [],
+    isLoading: false,
+    error: null,
+    currentPage: 1,
+    searchType: 'name',
+    searchTerm: '',
+    filterOption: 'review',
 
-  fetchCounselors: async (searchTerm:string|null) => {
-    // const { searchType, searchTerm, filterOption } = get();
-    set({ isLoading: true, error: null });
-    try {
-      const response = await axios.get(USERDEFAULT, {
-        params: searchTerm ? { name:searchTerm } : {}
-      });
-      set({ counselors: response.data, isLoading: false });
-      await console.log(response.data)
-    } catch (error) {
-      set({ error: 'Failed to fetch counselors', isLoading: false });
-    }
-  },
-  
+    fetchCounselors: async (searchTerm: string | null) => {
+        // const { searchType, searchTerm, filterOption } = get();
+        set({ isLoading: true, error: null });
+        try {
+            const response = await axios.get(USERDEFAULT, {
+                params: searchTerm ? { name: searchTerm } : {},
+            });
+            set({ counselors: response.data, isLoading: false });
+            await console.log(response.data);
+        } catch (error) {
+            set({ error: 'Failed to fetch counselors', isLoading: false });
+        }
+    },
 
     setSearchType: (type) => set({ searchType: type }),
     setSearchTerm: (term) => set({ searchTerm: term }),
@@ -70,16 +65,16 @@ const useCounselorStore = create<CounselorState>((set, get) => ({
 }));
 
 const sortCounselors = (counselors: Counselor[], option: string): Counselor[] => {
-  switch (option) {
-    case 'review':
-      return [...counselors].sort((a, b) => b.reviewCount - a.reviewCount);
-    case 'reviewAverage':
-      return [...counselors].sort((a, b) => b.reviewAverage - a.reviewAverage);
-    // case 'distance':
-    //   return [...counselors].sort((a, b) => a.distance - b.distance);
-    default:
-      return counselors;
-  }
+    switch (option) {
+        case 'review':
+            return [...counselors].sort((a, b) => b.reviewCount - a.reviewCount);
+        case 'reviewAverage':
+            return [...counselors].sort((a, b) => b.reviewAverage - a.reviewAverage);
+        // case 'distance':
+        //   return [...counselors].sort((a, b) => a.distance - b.distance);
+        default:
+            return counselors;
+    }
 };
 
 export default useCounselorStore;
