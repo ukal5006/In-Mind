560280d204b37eecfd2e4e94f9dc2d298bbc65ff
유현진 <a92046503@gmail.com>
2024-08-12 16:48:34 +0900

    [FE] style: login 화면 css 적용 완료
    login 화면 css 적용 완료

--- FE/src/components/BigLogo.tsx
+++ FE/src/components/BigLogo.tsx
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 import Logo from './Logo';
 import { useNavigate } from 'react-router-dom';
+import { colors } from '../theme/colors';
 
 const StyledLogo = styled(Logo)`
     font-size: 40px;
     margin-bottom: 30px;
+    color: ${colors.realDarkGreen}
 `;
 
 function BigLogo() {
