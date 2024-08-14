ec4d496f0fef9e92b070895fcae1ed521a224c99
김종원 <roista57@gmail.com>
2024-08-15 02:23:05 +0900

    [INFRA] docs: requirements.txt 수정, 포트 수정

--- AI/main.py
+++ AI/main.py
@@ -111,4 +111,4 @@ def download_image(url: str, save_path: str) -> None:
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=6000)
